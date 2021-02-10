f06d0ed25af89f4612026db66a6fbc847817b860
joelramirocc <www.joelramiro@gmail.com>
2021-02-09 19:20:31 -0600

    test example

--- Proyecto/plugin_base/plugin_main.cpp
+++ Proyecto/plugin_base/plugin_main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <easm.h>
+#include "rlutil.h" 
 
 extern "C" ErrorCode handleSyscall(uint32_t *regs, void *mem, MemoryMap *mem_map)
 {
@@ -12,6 +13,35 @@ extern "C" ErrorCode handleSyscall(uint32_t *regs, void *mem, MemoryMap *mem_map
             int a0 = regs[Register::a0];
             int a1 = regs[Register::a1];
             regs[Register::v0] = a0 + a1;
+            return ErrorCode::Ok;
+        }
+        case 21:
+        {
+            int a0 = regs[Register::a0];
+            int a1 = regs[Register::a1];
+            for (size_t i = 0; i < a0; i++)
+            {
+                rlutil::setColor(a1);
+        		std::cout << i << " ";
+            }
+            rlutil::resetColor();
+            return ErrorCode::Ok;
+        }
+        case 22:
+        {
+            int a0 = regs[Register::a0];
+            int a1 = regs[Register::a1];
+            for (size_t i = 0; i < a0; i++)
+            {
+                rlutil::setBackgroundColor(a1);
+        		std::cout << i << " ";
+            }
+            rlutil::resetColor();
+            return ErrorCode::Ok;
+        }
+        case 23:
+        {
+            rlutil::cls();
             return ErrorCode::Ok;
         }
         default:
