1ec362d46977e29af77658bb0468193272e56b65
joelramirocc <www.joelramiro@gmail.com>
2021-02-13 23:26:26 -0600

    se preparo ambiente para dibujar bloques

--- Proyecto/plugin_base/drawBlocks.asm
+++ Proyecto/plugin_base/drawBlocks.asm
@@ -4,6 +4,26 @@
 ;a1=>columna
 ;v0=true
 draw_blocks:
-    
-    jr $ra
+    li $t0,1
+    beq $a0,$t0,continue_draw
+    li $t0,2
+    beq $a0,$t0,continue_draw
+    li $t0,4
+    beq $a0,$t0,continue_draw
+    li $t0,5
+    beq $a0,$t0,continue_draw
+    li $t0,6
+    beq $a0,$t0,continue_draw
+    li $t0,8
+    beq $a0,$t0,continue_draw
+    li $t0,9
+    beq $a0,$t0,continue_draw
+    j end_function
+
+continue_draw:
+
+end_function:
+jr $ra
+
+
     
