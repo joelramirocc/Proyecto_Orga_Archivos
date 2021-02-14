7a41b300537337faac2a4f643a54db8dba248e61
joelramirocc <www.joelramiro@gmail.com>
2021-02-14 16:14:54 -0600

    se agrego logica para dibujar bloques

--- Proyecto/plugin_base/drawBlocks.asm
+++ Proyecto/plugin_base/drawBlocks.asm
@@ -1,8 +1,14 @@
 .global draw_blocks
 
+.data 
+Str1: .byte "[TTTTTTTT]", 0
+.text
+
 ;a0=>linea
 ;a1=>columna
+;a2=>arrayPos
 ;v0=true
+
 draw_blocks:
     li $t0,1
     beq $a0,$t0,continue_draw
@@ -18,12 +24,45 @@ draw_blocks:
     beq $a0,$t0,continue_draw
     li $t0,9
     beq $a0,$t0,continue_draw
+    li $v0,0
     j end_function
 
 continue_draw:
+    li $t0,3
+    beq $a1,$t0,continue_draw_block
+    li $t0,14
+    beq $a1,$t0,continue_draw_block
+    li $t0,25
+    beq $a1,$t0,continue_draw_block
+    li $t0,36
+    beq $a1,$t0,continue_draw_block
+    li $t0,47
+    beq $a1,$t0,continue_draw_block
+    li $t0,58
+    beq $a1,$t0,continue_draw_block
+    li $t0,69
+    beq $a1,$t0,continue_draw_block
+    li $t0,80
+    beq $a1,$t0,continue_draw_block
+    li $v0,0
+    j end_function
 
+continue_draw_block:
+    ;leer status del bloque, si es 0 significa que esta destruido y no se debe pintar
+    sll $t1, $a1,2
+    add $t1,$t1,$a2
+    lw $t1,0($a2)
+    li $v0,1
+    beq  $t1,$zero,end_function
+    ;si es diferente de 0, entonces toca dibujar el bloque segun el color que le toca(1-2-3)
+    ;el color representa el numero de golpes que necesita para destruirse
+        move $a0,$t1
+        li $v0,20
+        syscall
+        la $a0, Str1 
+        jal printString
+        li $v0,21
+        syscall
+        li $v0,1
 end_function:
 jr $ra
-
-
-    
--- Proyecto/plugin_base/principal.asm
+++ Proyecto/plugin_base/principal.asm
@@ -12,5 +12,6 @@ lw $ra,0($sp)
 addi $sp,$sp,4
 jr $ra
 
-;EasyASM --sc-handler ./b/libsc-plugin.so  --run easm_crt.asm principal.asm GetBlocks.asm drawLevel.asm
+;EasyASM --sc-handler ./b/libsc-plugin.so  --run easm_crt.asm principal.asm GetBlocks.asm drawBlocks.asm drawLevel.asm
 ;cd Proyecto_Orga_Archivos/Proyecto/plugin_base
+;pactl set-sink-volume @DEFAULT_SINK@ +10%
